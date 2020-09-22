9810650d39acacaaac631f84189fa3769b6e117f
JD Gardner <jdgardner@aptuscapitaladvisors.com>
2020-09-22 09:13:43 -0500

    working on assignment for callback props

--- src/List.js
+++ src/List.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import Card from './Card'
 import './List.css';
-
-function List(props) {
+export default function List(props) {
   return (
     <section className='List'>
       <header className='List-header'>
         <h2>{props.header}</h2>
       </header>
       <div className='List-cards'>
-        {props.cards.map((card, i) =>
+        {props.cards.map((card) =>
           <Card
-            key={i}
+            key={card.id}
+            id={card.id}
             title={card.title}
             content={card.content}
+            onClickDelete={props.onClickDelete}
           />
         )}
+        <button
+          type='button'
+          className='List-add-button'
+          onClick={() => props.onClickAdd(props.id)}
+        >
+          + Add Random Card
+        </button>
       </div>
     </section>
   )
 }
 
-export default List;
+List.defaultProps = {
+  onClickAdd: () => {},
+}
